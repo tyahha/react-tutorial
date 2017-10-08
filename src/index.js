474fea7d75bff9f2879c77890186705ed166486d
佐藤利徳 <tyahhahha@gmail.com>
2017-10-08 15:04:49 +0900

    finished Storing A History

--- src/index.js
+++ src/index.js
@@ -51,6 +51,7 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
@@ -60,7 +61,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -71,13 +72,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     const status = winner ?
                     `Winner: ${winner}` :
@@ -87,7 +96,7 @@ class Game extends React.Component {
         `Go to move #${move}`:
         'Go to game start';
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
